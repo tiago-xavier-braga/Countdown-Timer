609e54bec9dd290d246d14d47e99238b3724150c
Tiago Braga <braga.tiagoxavier@gmail.com>
2022-08-24 18:42:57 -0300

    Create date fuctions

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 var dateNow = new Date();
-var compromise = new Date(2022, 23, 18);
+var compromise = new Date(2022, 7, 25);
+var d = new Date();
 
-var d = new Date((compromise.setFullYear() - dateNow.setFullYear()), (compromise.setMonth() - dateNow.setMonth()), (compromise.setDate() - dateNow.setDate()));
+d.setFullYear(compromise.getFullYear() - dateNow.getFullYear());
+d.setMonth(compromise.getMonth() - dateNow.getMonth());
+d.setDate(compromise.getDate() - dateNow.getDate())
+d.setHours(compromise.getHours() - dateNow.getHours());
+d.setMinutes(compromise.getMinutes() - dateNow.getMinutes());
+d.setSeconds(compromise.getSeconds() - dateNow.getSeconds());
 
-console.log(d)
