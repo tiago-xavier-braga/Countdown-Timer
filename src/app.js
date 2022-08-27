9005e566abbd7f7b9fa15e0da75539dde1ffd166
Tiago Braga <braga.tiagoxavier@gmail.com>
2022-08-27 15:14:35 -0300

    Create box input

--- src/app.js
+++ src/app.js
@@ -1,13 +1,14 @@
-const dateNow = new Date();
-const compromise = new Date(2022, 8, 25, 17, 57);
-
-const gap = {
-    Year: Math.abs(compromise.getFullYear() - dateNow.getFullYear()),
-    Month: Math.abs(compromise.getMonth() - dateNow.getMonth()),
-    Day: Math.abs(compromise.getDate() - dateNow.getDate()),
-    Hours: Math.abs(compromise.getHours() - dateNow.getHours()),
-    Minutes: Math.abs(compromise.getMinutes() - dateNow.getMinutes()),
-    Secods: Math.abs(compromise.getSeconds() - dateNow.getSeconds()),
-};
-
-console.log(gap)
+function Start(){
+    const dateNow = new Date();
+    const compromise = new Date(document.getElementById('dateEvent').value);
+    const gap = {
+        Year: Math.abs(compromise.getFullYear() - dateNow.getFullYear()),
+        Month: Math.abs(compromise.getMonth() - dateNow.getMonth()),
+        Day: Math.abs(compromise.getDate() - dateNow.getDate()),
+        Hours: Math.abs(compromise.getHours() - dateNow.getHours()),
+        Minutes: Math.abs(compromise.getMinutes() - dateNow.getMinutes()),
+        Secods: Math.abs(compromise.getSeconds() - dateNow.getSeconds()),
+    };
+    
+    console.log(gap)
+}
